3f062af421804c6589f4f2a381aa53678cd951e5
booztot123e <tros2545@hotmail.com>
2024-02-16 14:42:23 +0700

    ed db env movie

--- dbconfig.js
+++ dbconfig.js
@@ -4,7 +4,7 @@ var dbconfig = {
     host: "localhost",
     port: "3306",
     user: "root",
-    password: "P@ssw0rd@2023",
+    password: "",
     database: "moviedb",
   },
   production: {
--- respository/movie.js
+++ respository/movie.js
@@ -1,176 +1,109 @@
-var mysql = require('mysql');
-const env = require('../env.js');
-const config = require('../dbconfig.js')[env];
-
-/*
-async function getMovieList() {
-
-    var Query;
-    var pool  = mysql.createPool(config);
-    
-    return new Promise((resolve, reject) => {
-
-       //Query = `SELECT * FROM movies WHERE warehouse_status = 1 ORDER BY CONVERT( warehouse_name USING tis620 ) ASC `;
-         Query = `SELECT * FROM movies`;
- 
-         pool.query(Query, function (error, results, fields) {
-            if (error) throw error;
-
-            if (results.length > 0) {
-                pool.end();
-                return resolve({
-                    statusCode: 200,
-                    returnCode: 1,
-                    data: results,
-                });   
-            } else {
-                pool.end();
-                return resolve({
-                    statusCode: 404,
-                    returnCode: 11,
-                    message: 'No movie found',
-                });
-            }
-
-        });
-
-    });
-    
-
-}
-*/
+var mysql = require("mysql");
+const env = require("../env.js");
+const config = require("../dbconfig.js")[env];
 
 async function getMovieList() {
+  var Query;
+  var pool = mysql.createPool(config);
 
-    var Query;
-    var pool = mysql.createPool(config);
+  return new Promise((resolve, reject) => {
+    //Query = `SELECT * FROM movies WHERE warehouse_status = 1 ORDER BY CONVERT( warehouse_name USING tis620 ) ASC `;
+    Query = `SELECT * FROM movies`;
 
-    return new Promise((resolve, reject) => {
+    pool.query(Query, function (error, results, fields) {
+      if (error) throw error;
 
-        //Query = `SELECT * FROM movies WHERE warehouse_status = 1 ORDER BY CONVERT( warehouse_name USING tis620 ) ASC `;
-        Query = `SELECT * FROM movies`;
-
-        pool.query(Query, function (error, results, fields) {
-            if (error) throw error;
-
-            if (results.length > 0) {
-                pool.end();
-                return resolve(results);
-            } else {
-                pool.end();
-                return resolve({
-                    statusCode: 404,
-                    returnCode: 11,
-                    message: 'No movie found',
-                });
-            }
-
+      if (results.length > 0) {
+        pool.end();
+        return resolve(results);
+      } else {
+        pool.end();
+        return resolve({
+          statusCode: 404,
+          returnCode: 11,
+          message: "No movie found",
         });
-
+      }
     });
-
-
+  });
 }
-
 
 async function getMovieSearch(search_text) {
+  var Query;
+  var pool = mysql.createPool(config);
 
-    var Query;
-    var pool = mysql.createPool(config);
+  return new Promise((resolve, reject) => {
+    Query = `SELECT * FROM movies WHERE title LIKE '%${search_text}%'`;
 
-    return new Promise((resolve, reject) => {
+    pool.query(Query, function (error, results, fields) {
+      if (error) throw error;
 
-        Query = `SELECT * FROM movies WHERE title LIKE '%${search_text}%'`;
-
-        pool.query(Query, function (error, results, fields) {
-            if (error) throw error;
-          
-            console.log("results: " + results)
-            console.log("results: " + JSON.stringify(results))
-            console.log("results.length: " + results.length)
-            
-            if (results.length > 0) {
-                pool.end();
-                return resolve({
-                    statusCode: 200,
-                    returnCode: 1,
-                    data: results,
-                });
-            } else {
-                pool.end();
-                return resolve({
-                    statusCode: 404,
-                    returnCode: 11,
-                    message: 'No movie found',
-                });
-            }
-
+      if (results.length > 0) {
+        pool.end();
+        return resolve({
+          statusCode: 200,
+          returnCode: 1,
+          data: results,
         });
-
+      } else {
+        pool.end();
+        return resolve({
+          statusCode: 404,
+          returnCode: 11,
+          message: "No movie found",
+        });
+      }
     });
-
-
+  });
 }
 
 async function postMovie(p_title, p_genre, p_director, p_release_year) {
+  var Query;
+  var pool = mysql.createPool(config);
 
-    var Query;
-    var pool = mysql.createPool(config);
+  return new Promise((resolve, reject) => {
+    //Query = `SELECT * FROM movies WHERE title LIKE '%${search_text}%'`;
 
-    return new Promise((resolve, reject) => {
+    var post = {
+      title: p_title,
+      genre: p_genre,
+      director: p_director,
+      release_year: p_release_year,
+    };
 
-        //Query = `SELECT * FROM movies WHERE title LIKE '%${search_text}%'`;
+    console.log("post is: ", post);
 
-        var post = {
-            title: p_title,
-            genre: p_genre,
-            director: p_director,
-            release_year: p_release_year
-        };
+    Query = "INSERT INTO movies SET ?";
+    pool.query(Query, post, function (error, results, fields) {
+      //pool.query(Query, function (error, results, fields) {
 
-        console.log('post is: ', post);
+      //  if (error) throw error;
 
-        Query = 'INSERT INTO movies SET ?';
-        pool.query(Query, post, function (error, results, fields) {
-            //pool.query(Query, function (error, results, fields) {
-
-            if (error) {
-                console.log("error: " + JSON.stringify(error))
-                pool.end();
-                return resolve({
-                    error: true,
-                    statusCode: 404,
-                    returnCode: 0,
-                    errMessage: error.code + ':' + error.sqlMessage
-                });
-
-            }
-            else
-            if (results.affectedRows > 0) {
-        
-                console.log("results: " + JSON.stringify(results))
-    
-                pool.end();
-                return resolve({
-                    error: false,
-                    statusCode: 200,
-                    returnCode: 1,
-                    messsage: 'Movie list was inserted',
-                });
-            }
-
-
+      if (error) {
+        console.log("error_code_msg", error.code + ": " + error.sqlMessage);
+        pool.end();
+        return resolve({
+          statusCode: 405,
+          returnCode: 9,
+          messsage: error.code + ": " + error.sqlMessage,
         });
-
-
+      } else {
+        console.log("results: ", results);
+        if (results.affectRows > 0) {
+          pool.end();
+          return resolve({
+            statusCode: 200,
+            returnCode: 1,
+            message: "Movie list was inserted",
+          });
+        }
+      }
     });
-
-
+  });
 }
 
 module.exports.MovieRepo = {
-    getMovieList: getMovieList,
-    getMovieSearch: getMovieSearch,
-    postMovie: postMovie,
-
+  getMovieList: getMovieList,
+  getMovieSearch: getMovieSearch,
+  postMovie: postMovie,
 };
